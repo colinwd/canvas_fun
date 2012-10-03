c1944eb82de9bd2c9441ee0682c4ece6f9a32a82
Colin Davis <cdavis@enviroissues.com>
2012-10-02 21:34:30 -0700

    return of the smiley!

--- js/main.js
+++ js/main.js
@@ -1,33 +1,42 @@
-var draw = function() {
+function draw() {
   var canvas = document.getElementById('awesome');
   if (canvas.getContext){
     var c = canvas.getContext('2d');
     c.fillStyle = "#FFFFFF";
     c.strokeStyle = "#FFFFFF";
 
-    for (var i = 0; i < 4; i++) {
-      for (var j=0; j < 3; j++) {
-        c.beginPath();
-        var x = 25 + j*50; // x coordinate
-        var y = 25 + i*50; // y coordinate
-        var radius = 20; // Arc radius
-        var startAngle = 0; // Starting point on circle
-        var endAngle = Math.PI+(Math.PI*j)/2; //End point on circle
-        var anticlockwise = i % 2 === 0 ? false : true; // clockwise or anticlockwise
+    var smileyFace = function(x, y) {
+      c.beginPath();
+      c.arc(75+x, 75+y, 50, 0, Math.PI*2, true); //head
+      c.moveTo(110+x,75+y);
+      c.arc(75+x, 75+y, 35, 0, Math.PI, false); //smile
+      c.moveTo(65+x, 65+y);
+      c.arc(60+x, 65+y, 5, 0, Math.PI*2, true); //left eye
+      c.moveTo(95+x, 65+y);
+      c.arc(90+x, 65+y, 5, 0, Math.PI*2, true); //right eye
+      c.stroke();
+    };
 
-        c.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+    smileyFace(300, 300);
 
-        if (i > 1) {
-          c.fill();
-        } else {
-          c.stroke();
-        }
-      }
-    }
+    var speechBubble = function(x, y) {
+      c.beginPath();
+      c.moveTo(75+x, 25+y);
+      c.quadraticCurveTo(25+x, 25+y, 25+x, 62.5+y);
+      c.quadraticCurveTo(25+x, 100+y, 50+x, 100+y);
+      c.quadraticCurveTo(50+x, 120+y, 30+x, 125+y);
+      c.quadraticCurveTo(60+x, 120+y, 65+x, 100+y);
+      c.quadraticCurveTo(125+x, 100+y, 125+x, 62.5+y);
+      c.quadraticCurveTo(125+x, 25+y, 75+x, 25+y);
+      c.stroke();
+    };
+
+    speechBubble(420, 250);
+
 
 
 
   } else {
     alert("fuck all yall");
   }
-};
+}
