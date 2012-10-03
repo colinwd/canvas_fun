480ebcde4cfca0badd645feb71d35eeb92a61e52
Colin Davis <cdavis@enviroissues.com>
2012-10-02 21:13:48 -0700

    Neat circles

--- js/main.js
+++ js/main.js
@@ -5,18 +5,25 @@ var draw = function() {
     c.fillStyle = "#FFFFFF";
     c.strokeStyle = "#FFFFFF";
 
-    c.beginPath();
-    c.moveTo(25, 25);
-    c.lineTo(105, 25);
-    c.lineTo(25, 105);
-    c.fill();
+    for (var i = 0; i < 4; i++) {
+      for (var j=0; j < 3; j++) {
+        c.beginPath();
+        var x = 25 + j*50; // x coordinate
+        var y = 25 + i*50; // y coordinate
+        var radius = 20; // Arc radius
+        var startAngle = 0; // Starting point on circle
+        var endAngle = Math.PI+(Math.PI*j)/2; //End point on circle
+        var anticlockwise = i % 2 === 0 ? false : true; // clockwise or anticlockwise
 
-    c.beginPath();
-    c.moveTo(125, 125);
-    c.lineTo(125, 45);
-    c.lineTo(45, 125);
-    c.closePath();
-    c.stroke();
+        c.arc(x, y, radius, startAngle, endAngle, anticlockwise);
+
+        if (i > 1) {
+          c.fill();
+        } else {
+          c.stroke();
+        }
+      }
+    }
 
 
 
