d559d298a47bddd7cf1dc4e7ba1bde43aa45ec0f
Colin Davis <cdavis@enviroissues.com>
2012-10-02 20:56:25 -0700

    triangles

--- js/main.js
+++ js/main.js
@@ -3,19 +3,21 @@ var draw = function() {
   if (canvas.getContext){
     var c = canvas.getContext('2d');
     c.fillStyle = "#FFFFFF";
-    c.strokeStyle = "#00FFFF";
+    c.strokeStyle = "#FFFFFF";
 
     c.beginPath();
-    c.arc(75, 75, 50, 0, Math.PI*2, true); //head
-    c.moveTo(55,85);
-    c.arc(75, 85, 20, 0, Math.PI, false); //mouth
-    c.moveTo(65, 65);
-    c.arc(60, 65, 5, 0, Math.PI*2, true); //left eye
-    c.moveTo(95, 65);
-    c.arc(90, 65, 5, 0, Math.PI*2, true); //right eye
-    c.moveTo(110,115);
-    c.arc(75, 115, 35, 0, Math.PI, true); //moustache
+    c.moveTo(25, 25);
+    c.lineTo(105, 25);
+    c.lineTo(25, 105);
+    c.fill();
+
+    c.beginPath();
+    c.moveTo(125, 125);
+    c.lineTo(125, 45);
+    c.lineTo(45, 125);
+    c.closePath();
     c.stroke();
+
 
 
   } else {
