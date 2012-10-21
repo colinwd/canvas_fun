4f175e462f88f90042ce90a5d60a18e4f6e0e2ff
Colin Davis <cdavis@enviroissues.com>
2012-10-21 13:02:44 -0700

    Paddle framing and bug fix

--- js/main.js
+++ js/main.js
@@ -6,11 +6,29 @@
   Crafty.e("Paddle, 2D, DOM, Color, Multiway")
     .color('rgb(255,0,0)')
     .attr({ x: 20, y: 100, w: 10, h: 50 })
-    .multiway(7, { W: -90, S: 90 } );
+    .multiway(7, { W: -90, S: 90 } )
+    .bind('EnterFrame', function() {
+      if (this.y <= 0) {
+        this.y = 0;
+      }
+
+      if (this.y >= 250) {
+        this.y = 250;
+      }
+    });
   Crafty.e("Paddle, 2D, DOM, Color, Multiway")
     .color('rgb(0,255,0)')
     .attr({ x: 580, y: 100, w: 10, h: 50 })
-    .multiway(7, { UP_ARROW: -90, DOWN_ARROW: 90});
+    .multiway(7, { UP_ARROW: -90, DOWN_ARROW: 90})
+    .bind('EnterFrame', function() {
+      if (this.y <= 0) {
+        this.y = 0;
+      }
+
+      if (this.y >= 250) {
+        this.y = 250;
+      }
+    });
 
     //Ball
     Crafty.e("2D, DOM, Color, Collision")
@@ -36,7 +54,7 @@
         //Left side
         if (this.x < 10) {
           this.x = 300;
-          this.dX = 3;
+          this.dX = -3;
           Crafty("RightPoints").each(function() {
             this.text(++this.points + " Points")
           });
